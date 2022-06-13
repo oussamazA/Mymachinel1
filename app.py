92f80771335731cd035ffe397e70818848215a1a
oussamazA <106214134+oussamazA@users.noreply.github.com>
2022-06-13 22:27:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,69 @@
-from flask import Flask, jsonify, request
+from flask import Flask, request, jsonify
+import pickle
+# import numpy as np
 import pandas as pd
-import numpy as np
-import joblib
-import traceback
-from flask_restful import reqparse
+
+model = pickle.load(open('finalmodel1.pkl', 'rb'))
+
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def hello():
-    return "hey"
 
-@app.route('/predict', methods=['POST'])
-def predict():
-	lr = joblib.load("model.pkl")
-	if lr:
-		try:
-			json = request.get_json()	 
-			model_columns = joblib.load("model_cols.pkl")
-			temp=list(json[0].values())
-			vals=np.array(temp)
-			prediction = lr.predict(temp)
-			print("here:",prediction)        
-			return jsonify({'prediction': str(prediction[0])})
+@app.route('/')
+def home():
+    return "These errors sucks!!!"
 
-		except:        
-			return jsonify({'trace': traceback.format_exc()})
-	else:
-		return ('No model here to use')
-    
+
+@app.route('/predictor', methods=['POST'])
+def predictor():
+    new = request.get_json()
+    marque = new['PC_Marque']
+    cpu_marque = new['CPU_Marque']
+    cpu_core = new['CPU_Core']
+    cpu_generation = new['CPU_Generation']
+    cpu_type = new['CPU_Type']
+    ram_horloge = new['CPU_Horloge']
+    ram_stockage = new['RAM_Stockage']
+    ram_type = new['RAM_Type']
+    ssd = new['SSD']
+    hdd = new['HDD']
+    gpu_marque = new['GPU_Marque']
+    gpu_type = new['GPU_Type']
+    gpu_version = new['GPU_Version']
+    gpu_categorie = new['GPU_Categorie']
+    gpu_capacité = new['GPU_Capacité']
+    ecran_taille = new['ECRAN_Taille']
+    ecran_resolution = new['ECRAN_Resolution']
+    ecran_type = new['ECRAN_Type']
+    etat = new['ETAT']
+
+    options = {
+        'PC_Marque': [marque],
+        'CPU_Marque': [cpu_marque],
+        'CPU_Core': [cpu_core],
+        'CPU_Generation': [cpu_generation],
+        'CPU_Type': [cpu_type],
+        'CPU_Horloge': [ram_horloge],
+        'RAM_Stockage': [ram_stockage],
+        'RAM_Type': [ram_type],
+        'SSD': [ssd],
+        'HDD': [hdd],
+        'GPU_Marque': [gpu_marque],
+        'GPU_Type': [gpu_type],
+        'GPU_Version': [gpu_version],
+        'GPU_Categorie': [gpu_categorie],
+        'GPU_Capacité': [gpu_capacité],
+        'ECRAN_Taille': [ecran_taille],
+        'ECRAN_Resolution': [ecran_resolution],
+        'ECRAN_Type': [ecran_type],
+        'ETAT': [etat],
+
+    }
+
+    input_query = pd.DataFrame(data=options)
+    result = model.predict(input_query)[0]
+
+    return jsonify(result.round().tolist(), 'DA')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="192.168.43.223", port=5111)
